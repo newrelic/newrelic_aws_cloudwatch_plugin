a57e4f015db44afaf48bfe488ff6e77dccace095
Sean Porter <portertech@gmail.com>
2013-04-29 23:51:32 +0000

    working overview toggle

--- lib/newrelic_aws.rb
+++ lib/newrelic_aws.rb
@@ -17,19 +17,20 @@ module NewRelicAWS
       end
 
       def agent_options
-        options = NewRelic::Plugin::Config.config.options
-        aws = options["aws"]
+        return @agent_options if @agent_options
+        @agent_options = NewRelic::Plugin::Config.config.options
+        aws = @agent_options["aws"]
         unless aws.is_a?(Hash) &&
             aws["access_key"].is_a?(String) &&
             aws["secret_key"].is_a?(String) &&
             aws["regions"].is_a?(Array)
           raise NewRelic::Plugin::BadConfig, "Missing or invalid AWS configuration."
         end
-        options
+        @agent_options
       end
 
       def overview_enabled?
-        agent_options["agents"][agent_name]["overview"]
+        !!agent_options["agents"][agent_name][:overview]
       end
 
       def setup_metrics
