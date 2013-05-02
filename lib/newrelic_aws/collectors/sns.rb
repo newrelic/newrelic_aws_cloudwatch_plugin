409e76850d0addb6496d2a60ba65a96d27a27d63
Sean Porter <portertech@gmail.com>
2013-05-02 17:56:16 +0000

    collect sns metrics

---
+++ lib/newrelic_aws/collectors/sns.rb
@@ -0,0 +1,46 @@
+module NewRelicAWS
+  module Collectors
+    class SNS < Base
+      def topic_names
+        sns = AWS::SNS.new(
+          :access_key_id => @aws_access_key,
+          :secret_access_key => @aws_secret_key,
+          :region => @aws_region
+        )
+        sns.topics.map { |topic| topic.name }
+      end
+
+      def metric_list
+        {
+          "NumberOfMessagesPublished"      => "Count",
+          "PublishSize"                    => "Bytes",
+          "NumberOfNotificationsDelivered" => "Count",
+          "NumberOfNotificationsFailed"    => "Count"
+        }
+      end
+
+      def collect
+        data_points = []
+        topic_names.each do |topic_name|
+          metric_list.each do |metric_name, unit|
+            data_point = get_data_point(
+              :namespace   => "AWS/SNS",
+              :metric_name => metric_name,
+              :unit        => unit,
+              :dimension   => {
+                :name  => "TopicName",
+                :value => topic_name
+              },
+              :period => 300,
+              :start_time => (Time.now.utc-360).iso8601
+            )
+            unless data_point.nil?
+              data_points << data_point
+            end
+          end
+        end
+        data_points
+      end
+    end
+  end
+end
