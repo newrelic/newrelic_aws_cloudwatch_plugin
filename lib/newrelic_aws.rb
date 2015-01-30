15d84374e26e1bae4bb58ba92b52c717818a958a
Mike McClurg <mike.mcclurg@gmail.com>
2015-01-30 16:24:46 -0700

    Add Redshift collector
    This commit adds a new collector for Redshift. All CloudWatch metrics
    are reported to NewRelic.
    
    Signed-off-by: Mike McClurg <mike.mcclurg@gmail.com>

--- lib/newrelic_aws.rb
+++ lib/newrelic_aws.rb
@@ -196,6 +196,14 @@ module NewRelicAWS
     end
   end
 
+  module RED
+    class Agent < Base::Agent
+      agent_guid "com.newrelic.aws.red"
+      agent_version NewRelicAWS::VERSION
+      agent_human_labels("Redshift") { "Redshift" }
+    end
+  end
+
   #
   # Register each agent with the component.
   #
@@ -208,6 +216,7 @@ module NewRelicAWS
   NewRelic::Plugin::Setup.install_agent :sns, SNS if NewRelicAWS::agent_enabled?(:sns)
   NewRelic::Plugin::Setup.install_agent :ec,  EC  if NewRelicAWS::agent_enabled?(:ec)
   NewRelic::Plugin::Setup.install_agent :ecr, ECR if NewRelicAWS::agent_enabled?(:ecr)
+  NewRelic::Plugin::Setup.install_agent :red, RED if NewRelicAWS::agent_enabled?(:red)
 
   #
   # Launch the agents; this never returns.
---
+++ lib/newrelic_aws/collectors/red.rb
@@ -0,0 +1,66 @@
+module NewRelicAWS
+  module Collectors
+    class RED < Base
+      def initialize(access_key, secret_key, region, options)
+        super(access_key, secret_key, region, options)
+      end
+
+      def clusters
+        return @clusters_ids if @clusters_ids
+        red = AWS::Redshift.new(
+          :access_key_id     => @aws_access_key,
+          :secret_access_key => @aws_secret_key,
+          :region            => @aws_region
+        )
+        begin
+          clusters = red.client.describe_clusters[:clusters]
+          @cluster_ids = clusters.map { |c| c[:cluster_identifier] }
+        rescue => e
+          STDERR.puts e.backtrace.join("\n")
+          return []
+        end
+      end
+
+      def metric_list
+        [
+          ["CPUUtilization", "Average", "Percent"],
+          ["DatabaseConnections", "Average", "Count"],
+          ["HealthStatus", "Average", "Count"],
+          ["MaintenanceMode", "Average", "Count"],
+          ["NetworkReceiveThroughput", "Average", "Bytes/Second"],
+          ["NetworkTransmitThroughput", "Average", "Bytes/Second"],
+          ["PercentageDiskSpaceUsed", "Average", "Percent"],
+          ["ReadIOPS", "Average", "Count/Second"],
+          ["ReadLatency", "Average", "Seconds"],
+          ["ReadThroughput", "Average", "Bytes/Second"],
+          ["WriteIOPS", "Average", "Count/Second"],
+          ["WriteLatency", "Average", "Seconds"],
+          ["WriteThroughput", "Average", "Bytes/Second"]
+        ]
+      end
+
+      def collect
+        data_points = []
+        clusters.each do |cluster_id|
+          metric_list.each do |(metric_name, statistic, unit)|
+            data_point = get_data_point(
+              :namespace   => "AWS/Redshift",
+              :metric_name => metric_name,
+              :statistic   => statistic,
+              :unit        => unit,
+              :dimension   => {
+                :name  => "ClusterIdentifier",
+                :value => cluster_id
+              }
+            )
+            NewRelic::PlatformLogger.debug("metric_name: #{metric_name}, statistic: #{statistic}, unit: #{unit}, response: #{data_point.inspect}")
+            unless data_point.nil?
+              data_points << data_point
+            end
+          end
+        end
+        data_points
+      end
+    end
+  end
+end
