e2a8f8c2010b86b7f51dc49dd025b04f6788f270
sestov <vsesto@odecee.com.au>
2016-10-03 10:04:27 +1100

    Adding Kinesis collector into the mix

--- lib/newrelic_aws/collectors/kin.rb
+++ lib/newrelic_aws/collectors/kin.rb
@@ -1,2 +1,63 @@
-# This is a kinesis collector
+module NewRelicAWS
+  module Collectors
+    class KIN < Base
+      def kinesis_stream
+        kin = Aws::Kinesis::Client.new(
+          :access_key_id => @aws_access_key,
+          :secret_access_key => @aws_secret_key,
+          :region => @aws_region,
+          :http_proxy => @aws_proxy_uri
+        )
+        kin.describe_streams.stream_name.map {|name| name }
+      end
 
+
+
+
+
+
+
+      def metric_list
+        [
+          ["Latency", "Average", "Seconds"],
+          ["RequestCount", "Sum", "Count", 0],
+          ["HealthyHostCount", "Maximum", "Count", 0],
+          ["UnHealthyHostCount", "Maximum", "Count", 0],
+          ["HTTPCode_ELB_4XX", "Sum", "Count", 0],
+          ["HTTPCode_ELB_5XX", "Sum", "Count", 0],
+          ["HTTPCode_Backend_2XX", "Sum", "Count", 0],
+          ["HTTPCode_Backend_3XX", "Sum", "Count", 0],
+          ["HTTPCode_Backend_4XX", "Sum", "Count", 0],
+          ["HTTPCode_Backend_5XX", "Sum", "Count", 0],
+          ["BackendConnectionErrors", "Sum", "Count", 0],
+          ["SurgeQueueLength", "Maximum", "Count", 0],
+          ["SpilloverCount", "Sum", "Count", 0]
+        ]
+      end
+
+      def collect
+        data_points = []
+        load_balancers.each do |load_balancer_name|
+          metric_list.each do |(metric_name, statistic, unit, default_value)|
+            data_point = get_data_point(
+              :namespace     => "AWS/ELB",
+              :metric_name   => metric_name,
+              :statistic     => statistic,
+              :unit          => unit,
+              :default_value => default_value,
+              :dimension     => {
+                :name  => "LoadBalancerName",
+                :value => load_balancer_name
+              }
+            )
+            NewRelic::PlatformLogger.debug("metric_name: #{metric_name}, statistic: #{statistic}, unit: #{unit}, response: #{data_point.inspect}")
+            unless data_point.nil?
+              data_points << data_point
+            end
+          end
+        end
+        data_points
+      end
+    end
+  end
+end
